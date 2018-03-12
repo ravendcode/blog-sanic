5f894eb49c48d9be67cd24685f39acebdc750bdc
ravendcode <ravendcode@gmail.com>
2018-03-13 01:11:31 +0200

    Add autoreconnect

--- static/js/app.js
+++ static/js/app.js
@@ -1,32 +1,45 @@
 (function () {
   'use strict';
 
-  var port = location.port === '' ? '' : ':' + location.port;
-  var wsUrl = 'ws://' + document.domain + port + '/echo';
-  if (/https/.test(location.href)) {
-    wsUrl = 'wss://' + document.domain  + '/echo';
+  function connect() {
+    var port = location.port === '' ? '' : ':' + location.port;
+    var wsUrl = 'ws://' + document.domain + port + '/echo';
+    if (/https/.test(location.href)) {
+      wsUrl = 'wss://' + document.domain + '/echo';
+    }
+
+    var echoWs = new WebSocket(wsUrl);
+
+    var messageFormElem = document.getElementById('message-form');
+    var messagesUlElem = document.getElementById('messages');
+    var messageInputElem = document.getElementById('message');
+
+
+    messageFormElem.onsubmit = function (event) {
+      event.preventDefault();
+
+      echoWs.send(JSON.stringify({
+        type: 'client',
+        data: messageInputElem.value
+      }));
+      messageInputElem.value = '';
+    };
+
+    echoWs.onmessage = function (event) {
+      var received = JSON.parse((event.data))
+      var liElem = document.createElement('li');
+      var content = document.createTextNode(received.type + ': ' + received.data);
+      liElem.appendChild(content);
+      messagesUlElem.appendChild(liElem);
+    };
+
+    echoWs.onclose = function (e) {
+      console.log('Socket is closed. Reconnect will be attempted in 1 second.', e.reason);
+      setTimeout(function () {
+        connect();
+      }, 1000);
+    };
   }
 
-  var echoWs = new WebSocket(wsUrl);
-
-  var messageFormElem = document.getElementById('message-form');
-  var messagesUlElem = document.getElementById('messages');
-  var messageInputElem = document.getElementById('message');
-
-
-  messageFormElem.onsubmit = function (event) {
-    event.preventDefault();
-
-    echoWs.send(JSON.stringify({type: 'client', data: messageInputElem.value}));
-    messageInputElem.value = '';
-  };
-
-  echoWs.onmessage = function (event) {
-    var received = JSON.parse((event.data))
-    var liElem = document.createElement('li');
-    var content = document.createTextNode(received.type + ': ' + received.data);
-    liElem.appendChild(content);
-    messagesUlElem.appendChild(liElem);
-  };
-
+  connect();
 }());
